ec02bbf7be0fd08426b660dc0b2b45444b9e82b9
Ming Xu <xming.xu@gmail.com>
2013-01-16 22:27:19 -0600

    try out defining a struct like data structure using field descriptor and enum

---
+++ cpp/LearnFunctional/MyFunctional.h
@@ -0,0 +1,91 @@
+#ifndef MYFUNCTIONAL_H_INCLUDED
+#define MYFUNCTIONAL_H_INCLUDED
+
+template <class E>
+struct Protocol {
+template <class T, E e>
+struct Field;
+
+template <typename... Ts>
+struct Message;
+
+template <class T, E e, typename... Ts>
+struct Message<Field<T, e>, Ts...> : Message<Ts...> {
+    template <E f, class S = void>
+    struct Value {
+        typedef typename Message<Ts...>::template Value<f>::type type;
+
+        static type get(Message<Ts...> const& m) {
+            return m.template get<f>();
+        }
+
+        static void set(Message<Ts...> & m, type const& v) {
+            m.template set<f>(v);
+        }
+    };
+
+    template <class S>
+    struct Value<e, S> {
+        typedef  T type;
+        typedef  S dummy;
+
+        static T get(Message<Field<T, e>, Ts...> const& m)  {
+            return m.value;
+        }
+
+        static void set(Message<Field<T, e>, Ts...> & m, T const& v) {
+            m.value = v;
+        }
+    };
+
+    T value;
+
+    template <E f>
+    typename Value<f>::type get() const {
+        return Value<f>::get(*this);
+    }
+
+    template <E f>
+    void set(typename Value<f>::type const& v) {
+        Value<f>::set(*this, v);
+    }
+};
+
+template <class T, E e>
+struct Message<Field<T, e>> {
+    template <E f, class S = void>
+    struct Value {
+        typedef void type;
+    };
+
+    T value;
+
+    template <class S>
+    struct Value<e, S> {
+        typedef T type;
+        typedef S dummy;
+
+        static T get(Message<Field<T, e>> const & m) {
+            return m.value;
+        }
+
+        static void set(Message<Field<T, e>> & m, T const& v) {
+            m.value = v;
+        }
+    };
+
+    template <E f>
+    typename Value<f>::type get() const {
+        return Value<f>::get(*this);
+    }
+
+    template <E f>
+    void set(typename Value<f>::type const& v) {
+        Value<f>::set(*this, v);
+    }
+};
+
+};
+
+
+#endif // MYFUNCTIONAL_H_INCLUDED
---
+++ cpp/LearnFunctional/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "MyFunctional.h"
+using namespace std;
+
+enum Logon {
+    Username,
+    Password,
+    Timestamp
+};
+
+typedef Protocol<Logon> LogonP;
+
+typedef LogonP::Message<LogonP::Field<const char*, Username>, LogonP::Field<int, Timestamp>> messageType;
+
+int main()
+{
+    const char* data = "hello world";
+    int a = 1;
+    messageType m;
+    m.set<Username>(data);
+    m.set<Timestamp>(a);
+
+
+    //message m;
+    //m(data, identity<Logon, Username>());
+    //t.getValue<Password>() = "secret";
+    //t.setValue<Timestamp>(1234);
+    cout << "value is: " << m.get<Username>() << endl;
+    cout << "value is: " << m.get<Timestamp>() << endl;
+    //cout << "value is: " << t.getValue<Timestamp>() << endl;
+    return 0;
+}
